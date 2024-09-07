c8f49680daebc42c72a208820233dac7b88ee662
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-08 00:27:10 +0530

    feat: init z-index && overflow

---
+++ src/layout/overflow.ts
@@ -0,0 +1,10 @@
+const overflow = {
+  visible: { overflow: 'visible' },
+  hidden: { overflow: 'hidden' },
+};
+
+// Example usage
+// console.log(overflow.visible); // { overflow: 'visible' }
+// console.log(overflow.hidden); // { overflow: 'hidden' }
+
+export default overflow;

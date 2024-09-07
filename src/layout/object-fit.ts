deede1df38aa5dbf418b43cbe6730c5479af05dd
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-07 22:00:08 +0530

    chore: types and concurrent bugs

--- src/layout/object-fit.ts
+++ src/layout/object-fit.ts
@@ -1,6 +1,6 @@
 import type { ResizeMode } from '../types/layout';
 
-const resizeModeValues: { [key: string]: ResizeMode } = {
+const resizeModeValues: { [key: string]: ResizeMode['resizeMode'] } = {
   cover: 'cover',
   contain: 'contain',
   stretch: 'stretch',
@@ -8,13 +8,13 @@ const resizeModeValues: { [key: string]: ResizeMode } = {
   center: 'center',
 };
 
-const getResizeModeValue = (key: string): { resizeMode: ResizeMode } => {
+const getResizeModeValue = (key: string): ResizeMode => {
   const value = resizeModeValues[key];
   if (value === undefined) throw new Error(`Invalid resize mode key: ${key}`);
   return { resizeMode: value };
 };
 
-const object_fit: { [key: string]: { resizeMode: ResizeMode } } = {};
+const object_fit: { [key: string]: ResizeMode } = {};
 
 Object.keys(resizeModeValues).forEach((key) => {
   object_fit[key] = getResizeModeValue(key);

b6bab9ef0ac476f20680d90d37aaf0682723f925
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-08 00:27:10 +0530

    feat: init z-index && overflow

---
+++ src/layout/z-index.ts
@@ -0,0 +1,20 @@
+const zIndex = {
+  z_0: { zIndex: 0 },
+  z_10: { zIndex: 10 },
+  z_20: { zIndex: 20 },
+  z_30: { zIndex: 30 },
+  z_40: { zIndex: 40 },
+  z_50: { zIndex: 50 },
+  z_auto: { zIndex: 'auto' },
+};
+
+// Example usage
+// console.log(zIndex.z_0); // { zIndex: 0 }
+// console.log(zIndex.z_10); // { zIndex: 10 }
+// console.log(zIndex.z_20); // { zIndex: 20 }
+// console.log(zIndex.z_30); // { zIndex: 30 }
+// console.log(zIndex.z_40); // { zIndex: 40 }
+// console.log(zIndex.z_50); // { zIndex: 50 }
+// console.log(zIndex.z_auto); // { zIndex: 'auto' }
+
+export default zIndex;

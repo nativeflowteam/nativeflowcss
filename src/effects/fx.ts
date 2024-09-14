4a931aa859ebee4755f9cca40a2ed9b10e5453d4
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-14 12:41:35 +0530

    chore: bg.ts -> fx.ts

--- src/effects/fx.ts
+++ src/effects/fx.ts
@@ -2,9 +2,9 @@ import colorList from '../utils/colorList';
 import type { ViewStyle, ImageStyle } from 'react-native';
 import type { BgStyles } from '../types/effects';
 
-const bg: BgStyles = {
+const fx: BgStyles = {
   // Custom background color
-  color_: (value: string): ViewStyle => ({
+  bg_color_: (value: string): ViewStyle => ({
     backgroundColor: value,
   }),
 
@@ -14,12 +14,12 @@ const bg: BgStyles = {
   }),
 
   // Custom opacity
-  opacity_: (value: number): ViewStyle => {
-    if (value < 0 || value > 1) {
+  opacity_: (value: number | string): ViewStyle => {
+    if (Number(value) < 0 || Number(value) > 1) {
       throw new Error('Opacity value must be between 0.0 and 1.0');
     }
     return {
-      opacity: value,
+      opacity: Number(value),
     };
   },
 
@@ -29,19 +29,17 @@ const bg: BgStyles = {
   }),
 
   // Custom elevation
-  elevation_: (value: number): ViewStyle => {
-    if (value < 1 || value > 10) {
+  elevation_: (value: number | string): ViewStyle => {
+    if (Number(value) < 1 || Number(value) > 10) {
       throw new Error('Elevation value must be between 1 and 10');
     }
     return {
-      elevation: value,
+      elevation: Number(value),
     };
   },
 
   box_auto: { pointerEvents: 'auto' },
   box_none: { pointerEvents: 'none' },
-  // box_only: { pointerEvents: 'auto' },
-  // box_auto: { pointerEvents: 'auto' },
 
   // Predefined backface visibility properties
   backface: { backfaceVisibility: 'visible' },
@@ -50,33 +48,33 @@ const bg: BgStyles = {
 
 // Predefined opacity properties (0-10)
 Array.from({ length: 11 }, (_, i) => i / 10).forEach((value) => {
-  bg[`opacity_${value * 10}`] = { opacity: value };
+  fx[`opacity_${value * 10}`] = { opacity: value };
 });
 
 // Predefined elevation properties (1-10)
 Array.from({ length: 10 }, (_, i) => i + 1).forEach((value) => {
-  bg[`elevation_${value}`] = { elevation: value };
+  fx[`elevation_${value}`] = { elevation: value };
 });
 
 // Dynamically add background color properties from colorList
 Object.keys(colorList).forEach((colorKey) => {
-  bg[`color_${colorKey}`] = {
+  fx[`bg_color_${colorKey}`] = {
     backgroundColor: colorList[colorKey],
   } as ViewStyle;
 });
 
 // Dynamically add tint color properties from colorList
 Object.keys(colorList).forEach((colorKey) => {
-  bg[`tint_${colorKey}`] = {
+  fx[`tint_${colorKey}`] = {
     tintColor: colorList[colorKey],
   } as ImageStyle;
 });
 
 // Dynamically add overlay color properties from colorList
 Object.keys(colorList).forEach((colorKey) => {
-  bg[`overlay_${colorKey}`] = {
+  fx[`overlay_${colorKey}`] = {
     overlayColor: colorList[colorKey],
   } as ViewStyle;
 });
 
-export default bg;
+export default fx;

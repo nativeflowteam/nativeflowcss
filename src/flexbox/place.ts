912c930f40301f2a2628a74149888313e5d266fb
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-14 20:07:52 +0530

    feat: place items property for flex && nits

---
+++ src/flexbox/place.ts
@@ -0,0 +1,32 @@
+const place = {
+  items_center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  items_stretch: {
+    alignItems: 'stretch',
+    justifyContent: 'stretch',
+  },
+  items_start: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  items_end: {
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  items_between: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  items_around: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  items_evenly: {
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+};
+
+export default place;

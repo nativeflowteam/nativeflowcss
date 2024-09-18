68fefbfb08fc857b3a5204eba84317f61f7f8b92
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-18 21:00:24 +0530

    chore: mixed nits (minor)

--- src/spacing/padding.ts
+++ src/spacing/padding.ts
@@ -3,13 +3,9 @@ import type { Padding } from '../types/spacing';
 const paddingValues: { [key: string]: number } = {
   '0': 0,
   'px': 1,
-  '0.5': 2,
   '1': 4,
-  '1.5': 6,
   '2': 8,
-  '2.5': 10,
   '3': 12,
-  '3.5': 14,
   '4': 16,
   '5': 20,
   '6': 24,
